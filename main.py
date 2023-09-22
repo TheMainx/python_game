59bd0fc70705489aee32678c304944b1a4df1aad
TheMainx <120021243+TheMainx@users.noreply.github.com>
2023-09-22 20:01:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 import random
+height = 10
+posy = 2
+posx = 2
 def room_generation(height, posy_player, posx_player):
     for i in range(height):
         if i == 0 or i == height - 1:
             print('*', (height - 2) * ' * ', '*')
         elif i + 1 == posy_player:
-            print('*', ' 0 ' * (posx_player - 2), 'x', ' 0 ' * (height - posx_player - 1), '*')
+            if i == (height - 2):
+                print('*', ' . ' * (posx_player - 2), 'x', ' . ' * (height - posx_player - 1), 'E')
+            else:
+                print('*', ' . ' * (posx_player - 2), 'x', ' . ' * (height - posx_player - 1), '*')
+        elif i == (height - 2):
+            print('*', (height - 2) * ' . ', 'E')
         else:
-            print('*', ' 0 ' * (height - 2), '*')
-
-def ruch(height, posy, posx):
+            print('*', ' . ' * (height - 2), '*')
+def move(height):
     x = input()
-    if x == "p":
-        if posx != height:
+    global posy
+    global posx
+    if (posx == height - 1) and (posy == height - 1):
+        print("WYGRAŁEŚ")
+    elif x == "p":
+        if posx != height -1:
             posx += 1
-        elif posx == height:
+        elif posx == height-1:
             print("zły ruch")
     elif x == "l":
-        if posx != 1:
+        if posx != 2:
             posx -= 1
-        elif posx == 1:
+        elif posx == 2:
             print("zły ruch")
     elif x == "g":
-        if posy != 1:
-            posy[1] -= 1
-        elif posy[1] == 1:
+        if posy != 2:
+            posy -= 1
+        elif posy == 2:
             print("zły ruch")
     elif x == "d":
-        if posy != height:
+        if posy < height - 1:
             posy += 1
-        elif posy == height:
+        elif posy == height -1:
             print("zły ruch")
-    elif posx == height and posy == height:
-        print("WYGRAŁEŚ")
-        
+
+while True:
+    room_generation(height, posy, posx)
+    move(height)
