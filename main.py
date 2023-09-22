371a7ea28f6f70cc5ce5dbfced3d3945553886ce
TheMainx <120021243+TheMainx@users.noreply.github.com>
2023-09-22 20:34:36 +0200

    Version_1 ready

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import random
+
 height = 10
 posy = 2
 posx = 2
+wyg = True
+
 def room_generation(height, posy_player, posx_player):
     for i in range(height):
         if i == 0 or i == height - 1:
             print('*', (height - 2) * ' * ', '*')
         elif i + 1 == posy_player:
             if i == (height - 2):
-                print('*', ' . ' * (posx_player - 2), 'x', ' . ' * (height - posx_player - 1), 'E')
+                print('*', ' . ' * (posx_player - 2), 'x', ' . ' * (height - posx_player - 2), 'E ', '*')
             else:
                 print('*', ' . ' * (posx_player - 2), 'x', ' . ' * (height - posx_player - 1), '*')
         elif i == (height - 2):
-            print('*', (height - 2) * ' . ', 'E')
+            print('*', (height - 3) * ' . ', 'E ', '*')
         else:
             print('*', ' . ' * (height - 2), '*')
+
+
 def move(height):
     x = input()
     global posy
     global posx
-    if (posx == height - 1) and (posy == height - 1):
-        print("WYGRAŁEŚ")
-    elif x == "p":
-        if posx != height -1:
+    global wyg
+    if x == "p":
+        if posx != height - 1:
             posx += 1
-        elif posx == height-1:
+            if (posx == height - 1) and (posy == height - 1):
+                print("WYGRAŁEŚ")
+                wyg = False
+
+        elif posx == height - 1:
             print("zły ruch")
     elif x == "l":
         if posx != 2:
@@ -39,9 +47,13 @@ def move(height):
     elif x == "d":
         if posy < height - 1:
             posy += 1
-        elif posy == height -1:
+            if (posx == height - 1) and (posy == height - 1):
+                print("WYGRAŁEŚ")
+                wyg = False
+        elif posy == height - 1:
             print("zły ruch")
 
-while True:
+while wyg:
     room_generation(height, posy, posx)
-    move(height)
+    if wyg == True:
+        move(height)
