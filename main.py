d66051c14877acfb06fa345dbdf271d9dab29db5
TheMainx <120021243+TheMainx@users.noreply.github.com>
2023-09-22 19:19:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-n = int(input())
-lista_zagniezdzona = [[0 for _ in range(n)] for _ in range(n)]
-l_g = [1, 1]
-czy_przeszedl = False
-while czy_przeszedl == False:
+import random
+def room_generation(height, posy_player, posx_player):
+    for i in range(height):
+        if i == 0 or i == height - 1:
+            print('*', (height - 2) * ' * ', '*')
+        elif i + 1 == posy_player:
+            print('*', ' 0 ' * (posx_player - 2), 'x', ' 0 ' * (height - posx_player - 1), '*')
+        else:
+            print('*', ' 0 ' * (height - 2), '*')
+
+def ruch(height, posy, posx):
     x = input()
     if x == "p":
-        if l_g[0] != n:
-            l_g[0] += 1
-        elif l_g[0] == n:
+        if posx != height:
+            posx += 1
+        elif posx == height:
             print("zły ruch")
     elif x == "l":
-        if l_g[0] != 1:
-            l_g[0] -= 1
-        elif l_g == 1:
+        if posx != 1:
+            posx -= 1
+        elif posx == 1:
             print("zły ruch")
     elif x == "g":
-        if l_g[1] != 1:
-            l_g[1] -= 1
-        elif l_g[1] == 1:
+        if posy != 1:
+            posy[1] -= 1
+        elif posy[1] == 1:
             print("zły ruch")
     elif x == "d":
-        if l_g[1] != n:
-            l_g[1] += 1
-        elif L_g[1] == n:
+        if posy != height:
+            posy += 1
+        elif posy == height:
             print("zły ruch")
+    elif posx == height and posy == height:
+        print("WYGRAŁEŚ")
+        
